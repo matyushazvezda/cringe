492e8005af4d2a5b07006f1aa8715e5daaa9b420
matyushazvezda <matyushazvezda@yandex.ru>
2023-05-24 12:48:16 +0800

    crud операции операции для concert

--- src/main/java/my_project/repository/MusicianRepository.java
+++ src/main/java/my_project/repository/MusicianRepository.java
@@ -2,12 +2,17 @@ package my_project.repository;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import my_project.dto.MusicianDTO;
+import my_project.model.Concert;
 import my_project.model.Musician;
 
 @Repository
 public interface MusicianRepository extends JpaRepository<Musician, Long> {
+
+    Optional<Concert> findAllById(MusicianDTO id);
 
 }
